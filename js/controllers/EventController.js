1bdf8cf42082e540af45a85fa6a2aada75d5049d
Stephen Young <stephenjayyoung@gmail.com>
2016-04-01 11:06:07 -0700

    finished the exercise

--- js/controllers/EventController.js
+++ js/controllers/EventController.js
@@ -1,5 +1,5 @@
 app.controller('EventController', ['$scope', 'events', '$routeParams', function($scope, events, $routeParams) {
   events.success(function(data) {
-    $scope.day = data[$routeParams.id];
+    $scope.detail = data.events[$routeParams.id];
   });
 }]);
